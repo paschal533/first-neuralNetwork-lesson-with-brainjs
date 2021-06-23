04f00aa4ee8738741b6116625e513c1ea4b892f7
unknown <okwuosahpashal@gmail.com>
2021-06-23 13:20:48 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const net = new brain.NeuralNetwork({
+    hiddenLayers: [4, 5, 6]
+});
+
+net.train([
+    {
+        input: [0, 0],
+        output: [0]
+    },
+    {
+        input: [1, 0],
+        output: [1]
+    },{
+        input: [0, 1],
+        output: [1]
+    },
+    {
+        input: [1, 1],
+        output: [0]
+    }
+])
+const diagram = document.getElementById('diagram');
+diagram.innerHTML = brain.utilities.toSVG(net);
